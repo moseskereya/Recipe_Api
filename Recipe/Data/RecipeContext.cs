7e9550739f91c493614f3c0e9942b1c8f80cf1b8
Moses <lazaromoses77@gmail.com>
2020-04-15 16:36:54 +0300

    Commits all

---
+++ Recipe/Data/RecipeContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Recipe.Model;
+
+namespace Recipe.Data
+{
+    public class RecipeContext :DbContext
+    {
+        public RecipeContext(DbContextOptions<RecipeContext> options) :base(options)
+        { }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
---
+++ Recipe/Model/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe.Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string type { get; set; }
+        public string description { get; set; }
+        public int Quality { get; set; }
+        public string Url { get; set; }
+    }
+}
